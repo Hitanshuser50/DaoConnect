6d3b2daaa27d4b9f0628703805ee5037cb05d4d9
blockvished <jcmvishael@gmail.com>
2025-06-14 14:51:54 +0000

    dao count

--- contracts/DAOConnect.sol
+++ contracts/DAOConnect.sol
@@ -5,13 +5,16 @@ import "./Dao.sol";
 
 contract DAOConnect {
     address[] public daos;
+    uint256 public daoCount = 0;
 
     event DAOCreated(address indexed daoAddress, string name, address creator);
 
     function createDAO(string memory _name) public {
         DAO dao = new DAO(_name, msg.sender);
         daos.push(address(dao));
+        daoCount+=1;
         emit DAOCreated(address(dao), _name, msg.sender);
+
     }
 
     function getAllDAOs() public view returns (address[] memory) {
