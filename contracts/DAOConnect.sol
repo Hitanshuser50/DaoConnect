3dc1d0f7113c742682d6635edb7d1120e62e6850
blockvished <jcmvishael@gmail.com>
2025-06-14 15:08:14 +0000

    amount requirement added

--- contracts/DAOConnect.sol
+++ contracts/DAOConnect.sol
@@ -6,15 +6,26 @@ import "./Dao.sol";
 contract DAOConnect {
     address[] public daos;
     uint256 public daoCount = 0;
+    uint256 public amountRequired = 0;
 
+    event AmountRequiredUpdated(uint256 newAmount);
     event DAOCreated(address indexed daoAddress, string name, address creator);
 
+    constructor(uint256 _amountRequired) {
+        amountRequired = _amountRequired;
+    }
+
+    function amountRequiredToCreateDao(uint256 _amt) external onlyOwner {
+        amountRequired = _amt;
+        emit AmountRequiredUpdated(_amt);
+    }
+
     function createDAO(string memory _name) public {
+        require(msg.value >= amountRequired, "Insufficient funds provided to create DAO");
         DAO dao = new DAO(_name, msg.sender);
         daos.push(address(dao));
         daoCount+=1;
         emit DAOCreated(address(dao), _name, msg.sender);
-
     }
 
     function getAllDAOs() public view returns (address[] memory) {
